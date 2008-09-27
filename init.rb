1b46d0a6e534074dc6215dc2ce5ec74486381b3d
John Trupiano <john@john-trupianos-macbook-pro.local>
2008-09-27 12:37:10 -0400

    Added basic documentation for new ActionController::Base#white_list_params to the README.

--- init.rb
+++ init.rb
@@ -1,4 +1,5 @@
 require 'white_lister'
 require 'white_list_helper'
+require 'white_list_parameter_filtering'
 ActionView::Base.send :include, WhiteListHelper
 ActionController::Base.send :include, WhiteListParameterFiltering
