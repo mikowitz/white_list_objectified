6fdabb92257974344d68231627a23e0c3f4e723f
Michael Berkowitz <michael.berkowitz@gmail.com>
2008-12-24 15:29:49 -0500

    Adds ActionController::Base#white_list_all_params function, basic documentation in the README, and notice in the CHANGELOG.

--- lib/white_list_parameter_filtering.rb
+++ lib/white_list_parameter_filtering.rb
@@ -8,4 +8,12 @@ module WhiteListParameterFiltering
       params[obj][method] = @@white_lister.white_list(params[obj][method])
     end
   end
+  
+  def white_list_all_params!(obj)
+    if !params[obj].nil? && params[obj].is_a?(Hash)
+      for key in params[obj].keys do
+        params[obj][key] = @@white_lister.white_list(params[obj][key]) if params[obj][key].is_a?(String)
+      end
+    end
+  end
 end
